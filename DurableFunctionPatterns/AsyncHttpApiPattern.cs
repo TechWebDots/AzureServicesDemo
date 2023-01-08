008a12e8cf6f8d299a627d79ed710e50fbb189ec
TechWebDots <TechWebDots@gmail.com>
2023-01-09 03:34:49 +0530

    AppInsight Reference Added

--- DurableFunctionPatterns/AsyncHttpApiPattern.cs
+++ DurableFunctionPatterns/AsyncHttpApiPattern.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using DurableTask.Core.Exceptions;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Azure.WebJobs.Extensions.Http;
@@ -11,32 +13,33 @@ namespace AzureServicesDemo.DurableFunctionPatterns
     public static class AsyncHttpApiPattern
     {
         #region Client Function for Deterministic API
-        [FunctionName("AsyncHttpApiPattern_HttpStart")]
+        [FunctionName("AsyncHttpApiPattern_HttpTrigger")]
         public static async Task<HttpResponseMessage> HttpStart(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
             [DurableClient] IDurableOrchestrationClient starter,
             ILogger log)
         {
             // Function input comes from the request content.
-            string instanceId = await starter.StartNewAsync("AsyncHttpApiPattern", null);
+            string instanceId = await starter.StartNewAsync("AsyncHttpApiPattern_OrchestrationTrigger", null);
 
-            log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+            log.LogInformation($"HttpTrigger Started orchestration with ID = '{instanceId}'.");
 
             return starter.CreateCheckStatusResponse(req, instanceId);
         }
         #endregion
 
         #region Orchestration Function
-        [FunctionName("AsyncHttpApiPattern")]
+        [FunctionName("AsyncHttpApiPattern_OrchestrationTrigger")]
         public static async Task<List<string>> RunOrchestrator(
-            [OrchestrationTrigger] IDurableOrchestrationContext context)
+            [OrchestrationTrigger] IDurableOrchestrationContext context, ILogger log)
         {
+            log.LogInformation($"OrchestrationTrigger Started.");
             var outputs = new List<string>();
 
             // Replace "hello" with the name of your Durable Activity Function.
-            outputs.Add(await context.CallActivityAsync<string>("AsyncHttpApiPattern_Hello", "Tokyo"));
-            outputs.Add(await context.CallActivityAsync<string>("AsyncHttpApiPattern_Hello", "Delhi"));
-            outputs.Add(await context.CallActivityAsync<string>("AsyncHttpApiPattern_Hello", "London"));
+            outputs.Add(await context.CallActivityAsync<string>("AsyncHttpApiPattern_ActivityTrigger", "Tokyo"));
+            outputs.Add(await context.CallActivityAsync<string>("AsyncHttpApiPattern_ActivityTrigger", "Delhi"));
+            outputs.Add(await context.CallActivityAsync<string>("AsyncHttpApiPattern_ActivityTrigger", "London"));                        
 
             //Correct way of using DateTime
             var currentUtcDateTime = context.CurrentUtcDateTime;
@@ -59,13 +62,25 @@ namespace AzureServicesDemo.DurableFunctionPatterns
         #endregion
 
         #region Activity Function Version 2
-        [FunctionName("AsyncHttpApiPattern_Hello")]
-        public static string SayHello([ActivityTrigger] IDurableActivityContext activityContext, ILogger log)
+        [FunctionName("AsyncHttpApiPattern_ActivityTrigger")]
+        public static string SayHelloActivity([ActivityTrigger] IDurableActivityContext activityContext, ILogger log)
         {
+            log.LogInformation($"ActivityTrigger Started.");
             string name = activityContext.GetInput<string>();
             log.LogInformation($"Saying hello to {name}.");
             return $"Hello {name}!";
         }
+        #endregion
+
+        #region Activity Function Version 3
+        //[FunctionName("AsyncHttpApiPattern_ActivityTrigger")]
+        //public static async Task<string> SayHelloActivity([ActivityTrigger] IDurableActivityContext activityContext, ILogger log)
+        //{
+        //    log.LogInformation($"ActivityTrigger Started.");
+        //    string name = activityContext.GetInput<string>();
+        //    log.LogInformation($"Saying hello to {name}.");
+        //    return $"Hello {name}!";
+        //}
         #endregion
     }
 }
